8fbfe547e0da34f5fc31e092b63e6ee3f528816d
vaibav <vaibav@vaibavs-MacBook-Air.local>
2024-11-09 22:37:40 +0530

    addingn

--- python.py
+++ python.py
@@ -1,9 +1,17 @@
 a = 10;
 b = 20;
-print("a",a)
-print("b",b)
-a = a+b;
-b = a - b;
-a = a - b;
-print(a);
-print(b)
+print("before swapping a: ",a)
+print("before swapping b: ",b)
+temp = a;
+a = b;
+b= temp
+
+# a = a+b;
+# b = a - b;
+# a = a - b;
+print('after swapping a: ',a);
+print('after swapping b: ',b)
+if(a!=b):
+    print('a is not equal to b')
+else:
+    print('a is equal to b')
